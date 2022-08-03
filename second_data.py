f65a5b9a97960cec618e788ea59b7aa94e377ac9
Safari <louisilva357@gmail.com>
2022-08-03 10:09:19 +0000

    read json object to dataframe

--- second_data.py
+++ second_data.py
@@ -4,6 +4,8 @@ import requests
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.stattools import adfuller
+import pmdarima as pmd
+
 
 
 def query(payload):
@@ -16,10 +18,10 @@ def query(payload):
     response =  requests.request("POST", url, headers=headers, data=payload)
     result = response.json()
     
-    return result
+    return pd.read_json(result)
 
 
-query("14125509")
+
 
 
 
@@ -27,10 +29,13 @@ query("14125509")
 #excel data in this instance
 
 
-def get_energy_data(filemame):
+def get_energy_data(file_name):
     
-    df=pd.read_excel(filename)
+    df=pd.read_excel(file_name)
     df['Consumption'] = abs(df['Balance'].diff().dropna())
+    df['Class'] = np.NaN
+    int(0.75*len(df['Consumption'])) 
+    df[df['date','Consumption','Class']]
     train = df[df["Class"] == "Train"]
     test = df[df["Class"] == "Test"]
     #creating train and test arrays 
@@ -39,18 +44,27 @@ def get_energy_data(filemame):
     
     #testing for stationarity
     #if p value is less than conventional 0.05 take difference
-    
+    #determination of p value for model
     pValue  = adfuller(train_array.dropna())[1]
-    while (pValue > 0.05):
-        p = 0
-        df['Consumption'].diff()
+    p = 0
+    if pValue > 0.05:
+        
+        df['Consumption'].diff().dropna()
         p = p+1
         
     else:
         p=0
     
-    
+    #Arima model
+    #p, order of auto-regressive model, q=order of the moving average model, d= the order of first-differencing
+    def arimamodel(timeseriesaarray):
+        auto_arima_model = pmd.auto_arima(timeseriesaarray,start_P=1,start_q=1,test="adf",trace=True)
         
+        return auto_arima_model
+    
+#using above function to build arima estimator on training data
+    arima_model = arimamodel(train_array)
+    
         
     
     
