b075346164f5ceae7f370aa8af4acbae1b676de1
Safari <louisilva357@gmail.com>
2022-08-03 00:33:23 +0000

    modified with comments

--- test_data.py
+++ test_data.py
@@ -15,18 +15,19 @@ class TestReadSdata(unittest.TestCase):
     def setUpClass(self):
         self.dataFrame = read_Sdata('^DJI')
         
-        
+#To ensure that all expected columns are present in the returned dataframe        
     def test_columnsPresent(self):
             self.assertIn("Open", self.df.columns)
             self.assertIn("High", self.df.columns)
             self.assertIn("Low", self.df.columns)
             self.assertIn("Close", self.df.columns)
             self.assertIn("Volume", self.df.columns)
-    
+
+#to ensure that there is more than one row of data    
     def test_non_empty(self):
         self.assertNotEqual(len(self.df.index),0)
         
-        
+#for high and low to be the actual highest and lowest in the same row       
     def test_highLow(self):
         open_high_close_low = self.df[["Open","High","Low","close"]]
         highest = open_high_close_low.max(axis=1)
@@ -34,7 +35,8 @@ class TestReadSdata(unittest.TestCase):
         self.assertTrue(open_high_close_low.le(highest, axis=0).all(axis=None))
         self.assertTrue(open_high_close_low.ge(lowest, axis=0).all(axis=None))
         
-        
+
+#to ensure that the most recent data was collected within the last week.        
     def test_most_recent_within_week(self):
         most_recent_date= pd.to_datetime(self.df.index[-1])
         self.assertLessEqual((datetime.datetime.today() - most_recent_date).days, 7)  
